4ab3757dae71b34ee4911a5f05df2f9b25ddadf0
JeongWhan Lee <jwlee95@kku.ac.kr>
2025-03-10 21:00:02 +0900

    Update SWV_printf.h

--- Utilities/SWV_printf.h
+++ Utilities/SWV_printf.h
@@ -1,3 +1,17 @@
+/*
+ * SWV_printf.h
+ *
+ *  Created on: Jan 22, 2025
+ *      Author: JeonghWhanPro
+ */
+
+#ifndef INC_SWV_PRINTF_H_
+#define INC_SWV_PRINTF_H_
+
+#include "main.h"
+#include <stdio.h>
+
+
 int _write(int file, char *ptr, int len) {
 int DataIdx;
 	for (DataIdx = 0; DataIdx < len; DataIdx++){
@@ -5,3 +19,6 @@ int DataIdx;
 	}
 	return len;
 }
+
+
+#endif /* INC_SWV_PRINTF_H_ */
