563455034ee6a1d52f4a0346462f57d80dc6de6a
JeongWhan Lee <jwlee95@kku.ac.kr>
2025-05-12 19:15:11 +0900

    Update waveform.c

--- Utilities/waveform.c
+++ Utilities/waveform.c
@@ -12,17 +12,17 @@
 
 void Wave_Init(WaveformTypeDef *wave, float Freq, float sr ){
     wave->F0 = Freq;
-    wave->T0 = 1.0/sr;   									// Sample interval....
+    wave->T0 = 1.0/sr;   					// Sample interval....
     wave->a1 = 2.0* cos( TWO_PI * wave->F0 * wave->T0 );  	// Initial Frequency: 100 Hz...
     wave->b0 = sin( TWO_PI * wave->F0 * wave->T0 );
-    wave->yn2 = 0.0; 										// Initial value: y[0] = 0
-    wave->yn1 = wave->b0;  									// Initial value: y[1] = b0
+    wave->yn2 = 0.0; 						// Initial value: y[0] = 0
+    wave->yn1 = wave->b0;  					// Initial value: y[1] = b0
 }
 
 float Wave_Forward(WaveformTypeDef *wave) {
 	wave->yn0 = wave->a1 * wave->yn1 - wave->yn2; 	// y[n] = a1*y[n-1] - y[n-2]
-	wave->yn2 = wave->yn1;        					// y[n-2] = y[n-1]
-	wave->yn1 = wave->yn0;        					// y[n-1] = y[n]
+	wave->yn2 = wave->yn1;        			// y[n-2] = y[n-1]
+	wave->yn1 = wave->yn0;        			// y[n-1] = y[n]
     return (wave->yn0);
 }
 
@@ -30,8 +30,8 @@ float Wave_Forward(WaveformTypeDef *wave) {
 void Wave_Reset(WaveformTypeDef *wave) {
 	wave->a1 = 2.0* cos( TWO_PI * wave->F0 * wave->T0 );  	// Initial Frequency: 100 Hz...
 	wave->b0 = sin( TWO_PI * wave->F0 * wave->T0 );
-	wave->yn2 = 0.0; 										// Initial value: y[0] = 0
-	wave->yn1 = wave->b0;  									// Initial value: y[1] = b0
+	wave->yn2 = 0.0; 					// Initial value: y[0] = 0
+	wave->yn1 = wave->b0;  					// Initial value: y[1] = b0
 }
 
 /**
@@ -42,27 +42,3 @@ float PseudoNoise(void) {
   rvalue = (rand()%1000 + 20) / 1000.0;
   return(rvalue);
 }
-
-
-
-//void f(){
-///* USER CODE BEGIN WHILE */
-//  int i=0;
-//  while (1)  {
-//        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-//        j= (i++)%50;
-//        printf("SWD printf!...%04d\n",j);
-//        fvalue1 = ForwardWave(&Wave1);
-//        fvalue2 = ForwardWave(&Wave2);
-//        fvalue3 = 0.3* fvalue2 + fvalue1;
-//        IIR4_filterBlock(&filter1,&fvalue3, &output, 1);
-//        // int IIR4_filterBlock( IIR4Type * pThis, float * pInput, float * pOutput, unsigned int count )
-//
-//        HAL_Delay(50);
-//    /* USER CODE END WHILE */
-//
-//}
-
-
-
-
